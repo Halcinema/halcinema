33b69bd6259e5379a4261e10973dba665265e820
hirotowatanabe <hiroto.private@gmail.com>
2017-12-06 12:30:06 +0900

    create file etc

---
+++ user/vote/index.php
@@ -0,0 +1,19 @@
+<?php
+include($_SERVER["DOCUMENT_ROOT"]."halcinema/user/login_session.php");
+$pageTitle = "人気投票";
+if($MemMail == ""){
+    header("Location: /halcinema/user/");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<div id="wrapper">
+<?php include($_SERVER["DOCUMENT_ROOT"]."halcinema/head.php"); ?>
+<body class="vote">
+    <div id="wrapper">
+       <?php include($_SERVER["DOCUMENT_ROOT"]."halcinema/user/header.php"); ?>
+       <p class="vote_test">test</p>
+    </div><!-- /#wrapper -->
+</body>
+</html>
