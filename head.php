8534ecf38ba820e6580784e7a3b8bf875afc1862
hirotowatanabe <hiroto.private@gmail.com>
2018-02-07 10:41:55 +0900

    add etc

--- head.php
+++ head.php
@@ -1,12 +1,13 @@
 <head>
-<meta charset="UTF-8">
-<meta http-equiv="content-script-type" content="text/javascript">
-<meta http-equiv="content-style-type" content="text/css">
-<link rel="stylesheet" href="/halcinema/css/style.css" type="text/css" />
-<link rel="stylesheet" href="/halcinema/css/admin.css" type="text/css" />
-<link rel="stylesheet" href="/halcinema/css/vote.css" type="text/css" />
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-<script type="text/javascript" src="/halcinema/js/jquery-1.11.2.min.js"></script>
-<script type="text/javascript" src="/halcinema/js/main.js"></script>
-<title><?php print $pageTitle; ?> | halcinema</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="content-script-type" content="text/javascript">
+    <meta http-equiv="content-style-type" content="text/css">
+    <link rel="stylesheet" href="/halcinema/css/style.css" type="text/css" />
+    <link rel="stylesheet" href="/halcinema/css/admin.css" type="text/css" />
+    <link rel="stylesheet" href="/halcinema/css/admin-sub.css" type="text/css" />
+    <link rel="stylesheet" href="/halcinema/css/vote.css" type="text/css" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <script type="text/javascript" src="/halcinema/js/jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="/halcinema/js/main.js"></script>
+    <title><?= $pageTitle ?> | halcinema</title>
 </head>
